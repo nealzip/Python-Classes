ac39a59537e9e4fbfa3e3d5bd714bc2783705ad2
Neal Zipper <Neal@nealZipper.com>
2017-04-01 15:26:21 -0400

    Initial commit

---
+++ class.py
@@ -0,0 +1,22 @@
+__author__ = 'nealzipper'
+# User: nealzipper
+# Date: 3/27/17
+# Time: 7:12 PM
+# Project Name: Python-Classes
+
+
+class MyStuff(object):
+
+	def __init__(self):
+		self.me = "Hi Neal"
+	def apple(self):
+		print("Steve Jobs would be proud")
+
+blah=MyStuff()
+
+print (blah.me)
+blah.apple()
+
+print('Done')
+
+
